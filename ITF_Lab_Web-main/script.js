9e301559f66353612e5afa319601cfc5a35d3997
Napapha Thoowasin <75508102+Napapha038@users.noreply.github.com>
2025-10-05 12:43:12 +0700

    Add files via upload

---
+++ ITF_Lab_Web-main/script.js
@@ -0,0 +1,9 @@
+const myinput = document.getElementById("myinput")
+
+function pos() {
+    myinput.value = Number(myinput.value) + 1
+}
+
+function neg() {
+    myinput.value = Number(myinput.value) - 1
+}
