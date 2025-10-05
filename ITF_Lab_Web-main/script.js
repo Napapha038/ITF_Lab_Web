5cfc9173c7d02757d10bd1fce2dfbeeb751bfb80
Napapha Thoowasin <75508102+Napapha038@users.noreply.github.com>
2025-10-05 12:45:43 +0700

    Delete ITF_Lab_Web-main directory

--- ITF_Lab_Web-main/script.js
+++
@@ -1,9 +0,0 @@
-const myinput = document.getElementById("myinput")
-
-function pos() {
-    myinput.value = Number(myinput.value) + 1
-}
-
-function neg() {
-    myinput.value = Number(myinput.value) - 1
-}
